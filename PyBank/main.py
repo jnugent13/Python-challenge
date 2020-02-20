e157131b166405df8075f89409b0ef80ed5b7657
Jessica Nugent <jessicanugent@Jessicas-MacBook-Air.local>
2020-02-19 22:35:52 -0800

    Updated code

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,31 +3,52 @@ import os
 
 csvpath = os.path.join("git/Python-challenge/Resources/budget_data.csv")
 
+# Lists to store data
+date = []
+profit = []
+
 # Open the csv file
-with open(csvpath, 'r') as csvfile:
+with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    for row in csvreader:
+        # Add date
+        date.append(row[1])
+        # Add profit/loss
+        profit.append(row[2])
 
-    csvheader = next(csvreader)
-
-    # Define variables for values
-    def print_values(budget_data):
-        month = str(budget_data[0])
-        profit = int(budget_data[1])
-
-        # Calculate number of months in dataset
-        month_list = []
-        for row in csvreader:
-            month_list.append(month)
-            return len(month_list)
+        # Get total months listed in dataset
+        def total_months(date):
+            months_count = len(date)
+            return months_count
 
         # Net total of profit and losses over time period
-    # Average in changes of profit and losses over time period
+        def total_profit(profit):
+            total = sum(profit)
+            return total
 
-    # Greatest increase in profits (date and amount) over time period
+        # Average in changes of profit and losses over time period
+        def average(profit):
+            length = len(profit)
+            total = sum(profit)
+            return total / length
 
-    # Greatest decrease in losses (date and amount) over time period
+        # Greatest increase in profits (date and amount) over time period
+        def greatest_increase(profit):
+            greatest_profit = max(profit)
+            return greatest_profit
 
-    # Print analysis to terminal
-    print(print_values(budget_data=csvreader))
+        # Greatest decrease in losses (date and amount) over time period
+        def greatest_decrease(profit):
+            greatest_loss = min(profit)
+            return greatest_loss
 
-    # Export text file with results 
+        # Print analysis to terminal
+        print('Financial Analysis')
+        print('-----------------------------------')
+        print(f'Total Months: {str(total_months(date))}')
+        print(f'Total: ${str(total_profit(profit))}')
+        print(f'Average Change: ${str(average(profit))}')
+        print(f'Greatest Increase in Profits: ${str(greatest_increase(profit))}')
+        print(f'Greatest Decrease in Profit: ${str(greatest_decrease(profit))}')
+
+        # Export text file with results 
