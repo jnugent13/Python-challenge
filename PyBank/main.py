a6c7a47562a56ff88075a14318c153eb8660b9de
Jessica Nugent <jessicanugent@Jessicas-MacBook-Air.local>
2020-02-21 20:12:03 -0800

    Finished calculations

--- PyBank/main.py
+++ PyBank/main.py
@@ -36,26 +36,26 @@ with open(csvpath) as csvfile:
         def average(profit_change):
             length = len(profit_change) - 1
             return '${:,.2f}'.format(total_change / length)
-
+        
         # Greatest increase in profits (date and amount) over time period
-        def greatest_increase(profit):
-            greatest_profit = max(profit)
-            if row[1] == greatest_profit:
-                greatest_profit_date = row[0]
-                return str(greatest_profit_date) + " ($" + str(greatest_profit) +")"
+        def greatest_increase(profit_change):
+            greatest_value = max(profit_change)
+            n = profit_change.index(greatest_value)
+            return str(date[n]) + " ($" + str(greatest_value) + ")"
 
         # Greatest decrease in losses (date and amount) over time period
-        def greatest_decrease(profit):
-            greatest_loss = min(profit)
-            return greatest_loss
+        def greatest_decrease(profit_change):
+            greatest_loss = min(profit_change)
+            n = profit_change.index(greatest_loss)
+            return str(date[n]) + " ($" + str(greatest_loss) + ")"
 
     # Print analysis to terminal
     print('Financial Analysis')
     print('-----------------------------------')
     print(f'Total Months: {str(months_count)}')
     print(f'Total: ${str(total_profit)}')
-    print(f'Average Change: ${str(average(profit))}')
-    print(f'Greatest Increase in Profits: ${str(greatest_increase(profit))}')
-    print(f'Greatest Decrease in Profit: ${str(greatest_decrease(profit))}')
+    print(f'Average Change: ${str(average(profit_change))}')
+    print(f'Greatest Increase in Profits: {str(greatest_increase(profit_change))}')
+    print(f'Greatest Decrease in Profit: {str(greatest_decrease(profit_change))}')
 
 # Export text file with results 
