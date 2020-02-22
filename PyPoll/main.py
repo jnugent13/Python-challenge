e40753dd2607a5d45034080b6ae7494544732fd5
Jessica Nugent <jessicanugent@Jessicas-MacBook-Air.local>
2020-02-21 21:19:36 -0800

    Updated code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,24 +5,34 @@ csvpath = os.path.join("Downloads", "election_data.csv")
 
 # Lists to store data
 votes = []
-
+candidates = []
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvpath, delimiter=",")
     header = next(csvreader)
 
     for row in csvreader:
-        # Create function to print values
-        def print_results(election_data):
-            vote = int(election_data[0])
-            votes.append(vote)
-            total_votes = sum(votes)
-            return total
+        # Add votes
+        votes.append(row[0])
+        # Add candidates
+        candidates.append(row[1])
 
+        #Get total votes
+        total_votes = sum(votes)
 
-            # Print analysis to terminal
-            print("Election Results")
-            print("-----------------------")
-            print("Total Votes: " + total_votes)
+        # Create list of unique candidates
+        def unique(candidates):
+            unique_list = []
+            for name in candidates:
+                if name not in unique_list:
+                    unique_list.append(name)
+            
+
+    # Print analysis to terminal
+    print("Election Results")
+    print("-----------------------")
+    print(f'Total Votes: {str(total_votes)}')
+    print("-----------------------")
+
 
 # Export text file with results
