0520381cb98496edb373f8ebae363ad49e366b7b
Jessica Nugent <jessicanugent@Jessicas-MacBook-Air.local>
2020-02-22 20:47:19 -0800

    Updated code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,8 +7,9 @@ csvpath = os.path.join("Resources", "election_data.csv")
 votes = []
 candidates = []
 
+# Open the csv file
 with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvpath, delimiter=",")
+    csvreader = csv.reader(csvfile, delimiter=",")
 
     # Skip the header
     header = next(csvfile)
@@ -17,33 +18,30 @@ with open(csvpath) as csvfile:
         # Add votes
         votes.append(int(row[0]))
         # Add candidates
-        candidates.append(str(row[0]))
+        candidates.append(row[2])
 
         #Get total votes
         total_votes = sum(votes)
 
         # Create list of unique candidates
-        def unique(candidates):
-            unique_list = []
-            for name in candidates:
-                if name not in unique_list:
-                    unique_list.append(name)
-            return unique_list
+        unique_list = []
+        for name in candidates:
+            if name not in unique_list:
+                unique_list.append(name)
         
         # Get votes for each candidate
         def cadidate_votes(candidate):
-            for candidate in candidates:
+            for candidate in unique_list:
                 candidate_total = 0
                 if row[2] == candidate:
                     candidate_total = candidate_total + int(row[0])
                 return candidate_total
-
 
     # Print analysis to terminal
     print("Election Results")
     print("-----------------------")
     print(f'Total Votes: {str(total_votes)}')
     print("-----------------------")
-
+    print(f'{str(unique_list)}')
 
 # Export text file with results
